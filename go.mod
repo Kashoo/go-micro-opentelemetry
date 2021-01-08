d20974273ed12d629b3f72d5ae5de222d251ffd4
Dominik Golonka <domgolonka@gmail.com>
2021-01-08 15:56:13 -0800

    first commit

--- go.mod
+++ go.mod
@@ -4,21 +4,21 @@ go 1.15
 
 require (
 	github.com/golang/protobuf v1.4.3 // indirect
-	github.com/google/go-cmp v0.5.4 // indirect
 	github.com/google/uuid v1.1.4 // indirect
 	github.com/micro/go-micro/v2 v2.9.1
 	github.com/miekg/dns v1.1.35 // indirect
-	github.com/stretchr/testify v1.6.1 // indirect
-	go.opencensus.io v0.22.5
+	github.com/opentracing/opentracing-go v1.1.0
+	go.opencensus.io v0.22.0 // indirect
+	go.opentelemetry.io/otel v0.15.0
+	go.opentelemetry.io/otel/sdk v0.15.0 // indirect
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
 	golang.org/x/sync v0.0.0-20201207232520-09787c993a3a // indirect
 	golang.org/x/sys v0.0.0-20210105210732-16f7687f5001 // indirect
 	golang.org/x/text v0.3.4 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
-	google.golang.org/genproto v0.0.0-20210106152847-07624b53cd92
+	google.golang.org/genproto v0.0.0-20210106152847-07624b53cd92 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- opentelemetry.go
+++ opentelemetry.go
@@ -2,149 +2,195 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
 	"fmt"
-
 	"github.com/micro/go-micro/v2/client"
-	log "github.com/micro/go-micro/v2/logger"
 	"github.com/micro/go-micro/v2/metadata"
+	"github.com/micro/go-micro/v2/registry"
 	"github.com/micro/go-micro/v2/server"
-	"go.opencensus.io/trace"
-	"go.opencensus.io/trace/propagation"
+	"github.com/opentracing/opentracing-go"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/label"
+	"go.opentelemetry.io/otel/trace"
+	"strings"
 )
 
-const (
-	// TracePropagationField is the key for the tracing context
-	// that will be injected in go-micro's metadata.
-	TracePropagationField = "X-Trace-Context"
-)
-
-// clientWrapper wraps an RPC client and adds tracing.
-type clientWrapper struct {
+type otWrapper struct {
+	ot trace.Tracer
 	client.Client
 }
 
-func injectTraceIntoCtx(ctx context.Context, span *trace.Span) context.Context {
-	spanCtx := propagation.Binary(span.SpanContext())
-	metadata.Set(ctx, TracePropagationField, base64.RawStdEncoding.EncodeToString(spanCtx))
-	return ctx
-}
-
-// Call implements client.Client.Call.
-func (w *clientWrapper) Call(
-	ctx context.Context,
-	req client.Request,
-	rsp interface{},
-	opts ...client.CallOption) (err error) {
-	t := newRequestTracker(req, ClientProfile)
-	ctx = t.start(ctx, true)
-
-	defer func() { t.end(ctx, err) }()
-
-	ctx = injectTraceIntoCtx(ctx, t.span)
-
-	err = w.Client.Call(ctx, req, rsp, opts...)
-	return
-}
-
-// Publish implements client.Client.Publish.
-func (w *clientWrapper) Publish(ctx context.Context, p client.Message, opts ...client.PublishOption) (err error) {
-	t := newEventTracker(p, ClientProfile)
-	ctx = t.start(ctx, true)
-
-	defer func() { t.end(ctx, err) }()
-
-	ctx = injectTraceIntoCtx(ctx, t.span)
-
-	err = w.Client.Publish(ctx, p, opts...)
-	return
-}
-
-// NewClientWrapper returns a client.Wrapper
-// that adds monitoring to outgoing requests.
-func NewClientWrapper() client.Wrapper {
-	return func(c client.Client) client.Client {
-		return &clientWrapper{c}
-	}
-}
-
-func getTraceFromCtx(ctx context.Context) *trace.SpanContext {
-	encodedTraceCtx, ok := metadata.Get(ctx, TracePropagationField)
+// StartSpanFromContext returns a new span with the given operation name and options. If a span
+// is found in the context, it will be used as the parent of the resulting span.
+func StartSpanFromContext(ctx context.Context, tracer trace.Tracer, name string, opts ...trace.SpanOption) (context.Context, trace.Span, error) {
+	md, ok := metadata.FromContext(ctx)
 	if !ok {
-		return nil
+		md = make(metadata.Metadata)
 	}
 
-	traceCtxBytes, err := base64.RawStdEncoding.DecodeString(encodedTraceCtx)
+	// Find parent span.
+	// First try to get span within current service boundary.
+	// If there doesn't exist, try to get it from go-micro metadata(which is cross boundary)
+	if parentSpan := trace.SpanFromContext(ctx); parentSpan != nil {
+		opts = append(opts, trace.(parentSpan))
+		//opts = append(opts, opentracing.ChildOf(parentSpan))
+	} else if spanCtx, err := tracer.Extract(opentracing.TextMap, opentracing.TextMapCarrier(md)); err == nil {
+		opts = append(opts, opentracing.ChildOf(spanCtx))
+	}
+
+	// allocate new map with only one element
+	nmd := make(metadata.Metadata, 1)
+
+	ctx, sp := tracer.Start(ctx, name, opts...)
+
+	//if err := sp.Tracer().Inject(sp.Context(), opentracing.TextMap, opentracing.TextMapCarrier(nmd)); err != nil {
+	//	return nil, nil, err
+	//}
+	propagation
+
+	propagator := otel.GetTextMapPropagator()
+	propagator.Inject(ctx,nmd)
+
+	for k, v := range nmd {
+		md.Set(strings.Title(k), v)
+	}
+
+	ctx = trace.ContextWithSpan(ctx, sp)
+	ctx = metadata.NewContext(ctx, md)
+	return ctx, sp, nil
+}
+
+func (o *otWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+	name := fmt.Sprintf("%s.%s", req.Service(), req.Endpoint())
+	ctx, span, err := StartSpanFromContext(ctx, o.ot, name)
 	if err != nil {
-		log.Errorf("Could not decode trace context: %s", err.Error())
-		return nil
+		return err
 	}
+	defer span.End()
+	if err = o.Client.Call(ctx, req, rsp, opts...); err != nil {
+		span.AddEvent(
+			"",
+			trace.WithAttributes(label.String("error", err.Error())))
 
-	spanCtx, ok := propagation.FromBinary(traceCtxBytes)
-	if !ok {
-		log.Errorf("Could not decode trace context from binary")
-		return nil
+		span.SetAttributes(label.String("error", err.Error()))
 	}
-
-	return &spanCtx
+	return err
 }
 
-// NewHandlerWrapper returns a server.HandlerWrapper
-// that adds tracing to incoming requests.
-func NewHandlerWrapper() server.HandlerWrapper {
-	return func(fn server.HandlerFunc) server.HandlerFunc {
-		return func(ctx context.Context, req server.Request, rsp interface{}) (err error) {
-			t := newRequestTracker(req, ServerProfile)
-			ctx = t.start(ctx, false)
+func (o *otWrapper) Stream(ctx context.Context, req client.Request, opts ...client.CallOption) (client.Stream, error) {
+	name := fmt.Sprintf("%s.%s", req.Service(), req.Endpoint())
+	ctx, span, err := StartSpanFromContext(ctx, o.ot, name)
+	if err != nil {
+		return nil, err
+	}
+	defer span.End()
+	stream, err := o.Client.Stream(ctx, req, opts...)
+	if err != nil {
+		span.AddEvent(
+			"",
+			trace.WithAttributes(label.String("error", err.Error())))
+		span.SetAttributes(label.String("error", err.Error()))
+	}
+	return stream, err
+}
 
-			defer func() { t.end(ctx, err) }()
+func (o *otWrapper) Publish(ctx context.Context, p client.Message, opts ...client.PublishOption) error {
+	name := fmt.Sprintf("Pub to %s", p.Topic())
+	ctx, span, err := StartSpanFromContext(ctx, o.ot, name)
+	if err != nil {
+		return err
+	}
+	defer span.End()
+	if err = o.Client.Publish(ctx, p, opts...); err != nil {
+		span.AddEvent(
+			"",
+			trace.WithAttributes(label.String("error", err.Error())))
+		span.SetAttributes(label.String("error", err.Error()))
+	}
+	return err
+}
 
-			spanCtx := getTraceFromCtx(ctx)
-			if spanCtx != nil {
-				ctx, t.span = trace.StartSpanWithRemoteParent(
-					ctx,
-					fmt.Sprintf("rpc/%s/%s/%s", ServerProfile.Role, req.Service(), req.Endpoint()),
-					*spanCtx,
-				)
-			} else {
-				ctx, t.span = trace.StartSpan(
-					ctx,
-					fmt.Sprintf("rpc/%s/%s/%s", ServerProfile.Role, req.Service(), req.Endpoint()),
-				)
+// NewClientWrapper accepts an open tracing Trace and returns a Client Wrapper
+func NewClientWrapper(ot trace.Tracer, name string) client.Wrapper {
+	return func(c client.Client) client.Client {
+		if ot == nil {
+			ot = otel.Tracer(name)
+		}
+		return &otWrapper{ot, c}
+	}
+}
+
+// NewCallWrapper accepts an opentelemetry Tracer and returns a Call Wrapper
+func NewCallWrapper(ot trace.Tracer, name string) client.CallWrapper {
+	return func(cf client.CallFunc) client.CallFunc {
+		return func(ctx context.Context, node *registry.Node, req client.Request, rsp interface{}, opts client.CallOptions) error {
+			if ot == nil {
+				ot = otel.Tracer(name)
 			}
+			name := fmt.Sprintf("%s.%s", req.Service(), req.Endpoint())
+			ctx, span, err := StartSpanFromContext(ctx, ot, name)
+			if err != nil {
+				return err
+			}
+			defer span.End()
+			if err = cf(ctx, node, req, rsp, opts); err != nil {
+				span.AddEvent(
+					"",
+					trace.WithAttributes(label.String("error", err.Error())))
+				span.SetAttributes(label.String("error", err.Error()))
 
-			err = fn(ctx, req, rsp)
-			return
+			}
+			return err
 		}
 	}
 }
 
-// NewSubscriberWrapper returns a server.SubscriberWrapper
-// that adds tracing to subscription requests.
-func NewSubscriberWrapper() server.SubscriberWrapper {
-	return func(fn server.SubscriberFunc) server.SubscriberFunc {
-		return func(ctx context.Context, p server.Message) (err error) {
-			t := newEventTracker(p, ServerProfile)
-			ctx = t.start(ctx, false)
-
-			defer func() { t.end(ctx, err) }()
-
-			spanCtx := getTraceFromCtx(ctx)
-			if spanCtx != nil {
-				ctx, t.span = trace.StartSpanWithRemoteParent(
-					ctx,
-					fmt.Sprintf("rpc/%s/pubsub/%s", ServerProfile.Role, p.Topic()),
-					*spanCtx,
-				)
-			} else {
-				ctx, t.span = trace.StartSpan(
-					ctx,
-					fmt.Sprintf("rpc/%s/pubsub/%s", ServerProfile.Role, p.Topic()),
-				)
+// NewHandlerWrapper accepts an opentelegry Tracer and returns a Handler Wrapper
+func NewHandlerWrapper(ot trace.Tracer, name string) server.HandlerWrapper {
+	return func(h server.HandlerFunc) server.HandlerFunc {
+		return func(ctx context.Context, req server.Request, rsp interface{}) error {
+			if ot == nil {
+				ot = otel.Tracer(name)
 			}
+			name := fmt.Sprintf("%s.%s", req.Service(), req.Endpoint())
+			ctx, span, err := StartSpanFromContext(ctx, ot, name)
+			if err != nil {
+				return err
+			}
+			defer span.End()
+			if err = h(ctx, req, rsp); err != nil {
+				span.AddEvent(
+					"",
+					trace.WithAttributes(label.String("error", err.Error())))
+				span.SetAttributes(label.String("error", err.Error()))
 
-			err = fn(ctx, p)
-			return
+			}
+			return err
 		}
 	}
 }
+
+// NewSubscriberWrapper accepts an opentelemetry Tracer and returns a Subscriber Wrapper
+func NewSubscriberWrapper(ot trace.Tracer) server.SubscriberWrapper {
+	return func(next server.SubscriberFunc) server.SubscriberFunc {
+		return func(ctx context.Context, msg server.Message) error {
+			name := "Sub from " + msg.Topic()
+			if ot == nil {
+				ot = otel.Tracer(name)
+			}
+			ctx, span, err := StartSpanFromContext(ctx, ot, name)
+			if err != nil {
+				return err
+			}
+			defer span.End()
+			if err = next(ctx, msg); err != nil {
+				span.AddEvent(
+					"",
+					trace.WithAttributes(label.String("error", err.Error())))
+				span.SetAttributes(label.String("error", err.Error()))
+
+			}
+			return err
+		}
+	}
+}
+
