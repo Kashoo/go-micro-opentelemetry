1d9175d8c3737e319b19e6f2458bc350f221baf5
Dominik Golonka <domgolonka@gmail.com>
2021-01-12 12:24:38 -0800

    updates

---
+++ tracker.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.opentelemetry.io/otel/trace"
+	"time"
+)
+
+type tracker struct {
+	startedAt time.Time
+	tracer trace.Tracer
+	//profile *StatsProfile
+	span    trace.Span
+
+	method  string
+	service string
+}
+
+type requestDescriptor interface {
+	Service() string
+	Endpoint() string
+}
+
+// newRequestTracker creates a new tracker for an RPC request (client or server).
+func newRequestTracker(req requestDescriptor, tracer trace.Tracer) *tracker {
+	return &tracker{
+		tracer: tracer,
+		//profile: profile,
+		method:  req.Endpoint(),
+		service: req.Service(),
+	}
+}
+
+type publicationDescriptor interface {
+	Topic() string
+}
+
+// newEventTracker creates a new tracker for a publication (client or server).
+func newEventTracker(pub publicationDescriptor, tracer trace.Tracer) *tracker {
+	return &tracker{
+		tracer: tracer,
+		//profile: profile,
+		method:  pub.Topic(),
+		service: "pubsub",
+	}
+}
+
+// start monitoring a request. You can choose to let this method
+// start a span for the request or attach one later.
+func (t *tracker) start(ctx context.Context, startSpan bool) context.Context {
+	t.startedAt = time.Now()
+
+	//ctx, _ = tag.New(ctx, tag.Upsert(Service, t.service), tag.Upsert(Endpoint, t.method))
+	//stats.Record(ctx, t.profile.CountMeasure.M(1))
+
+	if startSpan {
+		ctx, t.span = t.tracer.Start(
+			ctx,
+			fmt.Sprintf("rpc/%s/%s", t.service, t.method),
+		)
+	}
+
+	return ctx
+}
+
+// end a request's monitoring session. If there is a span ongoing, it will
+// be ended and metrics will be recorded.
+func (t *tracker) end(ctx context.Context, err error) {
+	//status := getResponseStatus(err)
+
+	//ctx, _ = tag.New(ctx, tag.Upsert(StatusCode, strconv.Itoa(int(status.Code))))
+	//stats.Record(ctx, t.profile.LatencyMeasure.M(float64(time.Since(t.startedAt))/float64(time.Millisecond)))
+
+	if t.span != nil {
+		setResponseStatus(t.span,err, "")
+		//t.span.SetStatus(status)
+		t.span.End()
+	}
+}
